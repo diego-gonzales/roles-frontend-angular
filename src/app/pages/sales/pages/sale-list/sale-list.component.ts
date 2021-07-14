813df4d22ac286ea4cfa9dd7faffe49834ea06c7
Diego Gonzales <dgonzalesj14@unc.edu.pe>
2021-07-14 16:24:26 -0500

    Se agregó paginator de material a la tabla de ventas

--- src/app/pages/sales/pages/sale-list/sale-list.component.ts
+++ src/app/pages/sales/pages/sale-list/sale-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { SaleService } from '../../services/sale.service';
 import { Sale } from '../../interfaces/sale-response.interface';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-sale-list',
@@ -9,7 +11,26 @@ import { Sale } from '../../interfaces/sale-response.interface';
 })
 export class SaleListComponent implements OnInit {
 
+  // sales: Sale[] = [];
+  
+  // constructor( private saleService: SaleService ) { }
+  
+  // ngOnInit(): void {
+  //   this.getSalesAndFillArray();
+  // };
+
+  // getSalesAndFillArray(): void {
+  //   this.saleService.getSales()
+  //       .subscribe( resp => {
+  //         this.sales = resp.reverse();
+  //       });
+  // };
+
   sales: Sale[] = [];
+  displayedColumns: string[] = ['createdAt', 'customer', 'user', 'totalPrice', '_id'];
+  dataSource = new MatTableDataSource<Sale>(this.sales);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
   constructor( private saleService: SaleService ) { }
@@ -23,7 +44,8 @@ export class SaleListComponent implements OnInit {
     this.saleService.getSales()
         .subscribe( resp => {
           this.sales = resp.reverse();
+          this.dataSource = new MatTableDataSource<Sale>(this.sales);
+          this.dataSource.paginator = this.paginator;
         });
   };
-
 }
